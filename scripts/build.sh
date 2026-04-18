cb919e3033453ccbef11c2878720f06f9eac5af4
Terence Pires <terence.pires@gmail.com>
2026-04-18 12:04:47 +0200

    update node-builder image

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,5 +9,6 @@ IMG_URL="titiyoyo/$I:$T"
 echo ">> building image $IMG_URL in $IMG_PATH"
 cd "$IMG_PATH"
 docker build \
+  --debug \
   --build-arg PHP_IMAGE_VERSION="$PHP_IMAGE_VERSION" \
   . -t "$IMG_URL"
